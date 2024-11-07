67c4d4c705bfd4964e3a4606d3734a8750272a50
premsai-noolu <premsainoolu01@gmail.com>
2024-11-07 10:05:45 +0530

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NavBar from "./NavBar";
+import Body from "./Body";
+import Login from "./Login";
+import Profile from "./Profile";
 
 function App() {
   return (
     <>
-      <NavBar />
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/profile" element={<Profile />}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
