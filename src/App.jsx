8112e2c489912d39b020c95ee40100d46488d14f
premsai-noolu <premsainoolu01@gmail.com>
2024-11-08 11:42:12 +0530

    Redux store and slices

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,22 @@ import NavBar from "./NavBar";
 import Body from "./Body";
 import Login from "./Login";
 import Profile from "./Profile";
+import { Provider } from "react-redux";
+import appStore from "./utils/appStore";
 
 function App() {
   return (
     <>
-      <BrowserRouter basename="/">
-        <Routes>
-          <Route path="/" element={<Body />}>
-            <Route path="/login" element={<Login />}></Route>
-            <Route path="/profile" element={<Profile />}></Route>
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <Provider store={appStore}>
+        <BrowserRouter basename="/">
+          <Routes>
+            <Route path="/" element={<Body />}>
+              <Route path="/login" element={<Login />}></Route>
+              <Route path="/profile" element={<Profile />}></Route>
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </>
   );
 }
--- src/Login.jsx
+++ src/Login.jsx
@@ -1,7 +1,62 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [emailId, setEmailId] = useState("");
+  const [passWord, setPassWord] = useState("");
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post(
+        "http://localhost:9999/login",
+        {
+          emailId,
+          passWord,
+        },
+        {
+          withCredentials: true,
+        }
+      );
+      console.log(res.data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  return (
+    <div>
+      <div className="card bg-base-100 image-full w-96 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title justify-center">login</h2>
+          <label className="form-control w-full max-w-xs">
+            <div className="label">
+              <span className="label-text">Email</span>
+            </div>
+            <input
+              type="text"
+              placeholder=""
+              value={emailId}
+              className="input input-bordered w-full max-w-xs mb-2"
+              onChange={(e) => setEmailId(e.target.value)}
+            />
+            <div className="label">
+              <span className="label-text">Password</span>
+            </div>
+            <input
+              type="text"
+              placeholder=""
+              value={passWord}
+              className="input input-bordered w-full max-w-xs mb-2"
+              onChange={(e) => setPassWord(e.target.value)}
+            />
+          </label>
+          <div className="card-actions justify-center my-3">
+            <button className="btn btn-primary" onClick={handleLogin}>
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
