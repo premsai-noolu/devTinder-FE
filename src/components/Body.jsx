34ea8a57a072b5670dc8bb7eda1bfd7ad14bc978
premsai-noolu <premsainoolu01@gmail.com>
2024-11-11 21:43:01 +0530

    redirect to login if not authenticated

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
 import NavBar from "./NavBar";
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet, useNavigate } from "react-router-dom";
 import Footer from "./Footer";
+import { Base_URL } from "../utils/constants";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { addUser } from "../utils/userSlice";
 
 const Body = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const userData = useSelector((store) => store.user);
+  const fetchUser = async () => {
+    if (userData) return;
+    try {
+      const res = await axios.get(Base_URL + "profile/view", {
+        withCredentials: true,
+      });
+      console.log("res" + res);
+      dispatch(addUser(res.data));
+    } catch (err) {
+      if (err.status === 401) {
+        navigate("/login");
+      }
+      console.log(err);
+    }
+  };
+  useEffect(() => {
+    fetchUser();
+  });
+
   return (
     <div>
       <NavBar />
