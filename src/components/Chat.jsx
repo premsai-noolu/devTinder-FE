23a83f56dda43a71e743bc38ed862632d421c45d
premsai-noolu <premsainoolu01@gmail.com>
2025-02-14 11:18:29 +0530

    Chat Feauture

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
 
 const Chat = () => {
   const { targetUserId } = useParams();
-  return <div>Chat</div>;
+  const [messages, setMessages] = useState([{ text: "Hello world" }]);
+
+  console.log(targetUserId);
+  return (
+    <div className="w-1/2 mx-auto border border-gray-600 m-5 flex flex-col h-[80vh]">
+      <h1 className="p-5 border-b border-gray-600">Chat</h1>
+      <div className="flex-1 overflow-scroll p-5">
+        {messages.map((msg, index) => {
+          return (
+            <div key={index} className="chat chat-start">
+              <div className="chat-header">
+                Prem Sai
+                <time className="text-xs opacity-50">2 hours ago</time>
+              </div>
+              <div className="chat-bubble">You were the Chosen One!</div>
+              <div className="chat-footer opacity-50">Seen</div>
+            </div>
+          );
+        })}
+      </div>
+      <div className="p-5 border-t border-gray-600 flex items-center gap-2">
+        <input className="flex-1 border border-gray-500 text-white rounded p-2"></input>
+        <button className="btn btn-secondary">Send</button>
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
