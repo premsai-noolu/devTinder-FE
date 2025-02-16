2415b22daecd95828da5d5a6b84de966e20c183f
premsai-noolu <premsainoolu01@gmail.com>
2025-02-16 10:41:31 +0530

    Implemented socket on chat feature

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -5,37 +5,67 @@ import { useSelector } from "react-redux";
 
 const Chat = () => {
   const { targetUserId } = useParams();
-  const [messages, setMessages] = useState([{ text: "Hello world" }]);
+  const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState("");
   const user = useSelector((store) => store.user);
   const userId = user?._id;
 
   useEffect(() => {
+    if (!userId) {
+      return;
+    }
     const socket = createSocketConnection();
-    socket.emit("joinChat", { userId, targetUserId });
+    socket.emit("joinChat", {
+      firstName: user.firstName,
+      userId,
+      targetUserId,
+    });
+
+    socket.on("messageRecieved", ({ firstName, text }) => {
+      console.log(firstName + ": " + text);
+      setMessages((messages) => [...messages, { firstName, text }]);
+    });
     return () => {
       socket.disconnect();
     };
-  }, []);
+  }, [userId, targetUserId]);
+
+  const sendMessage = () => {
+    const socket = createSocketConnection();
+    socket.emit("sendMessage", {
+      firstName: user.firstName,
+      userId,
+      targetUserId,
+      text: newMessage,
+    });
+    setNewMessage("");
+  };
   return (
-    <div className="w-1/2 mx-auto border border-gray-600 m-5 flex flex-col h-[80vh]">
+    <div className="w-3/4 mx-auto border border-gray-600 m-5 flex flex-col h-[80vh]">
       <h1 className="p-5 border-b border-gray-600">Chat</h1>
       <div className="flex-1 overflow-scroll p-5">
         {messages.map((msg, index) => {
           return (
             <div key={index} className="chat chat-start">
               <div className="chat-header">
-                Prem Sai
+                {msg.firstName}
                 <time className="text-xs opacity-50">2 hours ago</time>
               </div>
-              <div className="chat-bubble">You were the Chosen One!</div>
+              <div className="chat-bubble">{msg.text}</div>
               <div className="chat-footer opacity-50">Seen</div>
             </div>
           );
         })}
       </div>
       <div className="p-5 border-t border-gray-600 flex items-center gap-2">
-        <input className="flex-1 border border-gray-500 text-white rounded p-2"></input>
-        <button className="btn btn-secondary">Send</button>
+        <input
+          value={newMessage}
+          onChange={(e) => setNewMessage(e.target.value)}
+          className="flex-1 border border-gray-500 text-white rounded p-2"
+        ></input>
+        <button onClick={sendMessage} className="btn btn-secondary">
+          Send
+        </button>
       </div>
     </div>
   );
